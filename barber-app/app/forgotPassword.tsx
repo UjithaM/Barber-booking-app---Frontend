530bce59873d061219eee36c0e0a128db4b46871
Ujitha <ujithamigara@gmail.com>
2024-07-16 13:03:09 +0530

    add forgotPassword.tsx and ui

---
+++ barber-app/app/forgotPassword.tsx
@@ -0,0 +1,72 @@
+import {SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View} from "react-native";
+import {MaterialCommunityIcons} from "@expo/vector-icons";
+
+export  default  function () {
+
+    function onPressSend() {
+
+    }
+
+    return (
+        <SafeAreaView>
+            <View style={{flexDirection:'row', justifyContent: 'center', alignItems: "center", height: '100%'}}>
+                <View style={{width:'100%', padding: 20}}>
+                    <Text style={{fontSize:35, fontWeight: 'bold', color:'#363062'}}>
+                        Forgot Password?
+                    </Text>
+                    <Text style={{fontSize:18, color:'#6B7280', marginTop: 10}}>
+                        Please enter your email for the password reset process
+                    </Text>
+                    <Text style={styles.inputText}>
+                        Email
+                    </Text>
+                    <View style={styles.inputContainer}>
+                        <MaterialCommunityIcons
+                            name="email"
+                            size={24}
+                            color="#363062"
+                            style={{marginLeft: 10}}
+                        />
+                        <TextInput
+                            style={styles.input}
+                            placeholder="Enter Email"
+                            placeholderTextColor="#aaa"/>
+                    </View>
+                    <TouchableOpacity style={styles.loginButton} onPress={onPressSend}>
+                        <Text style={styles.loginButtonText}>Send</Text>
+                    </TouchableOpacity>
+                </View>
+
+            </View>
+        </SafeAreaView>
+    )
+}
+const styles = StyleSheet.create({
+    inputText: {
+        marginTop: 20,
+    },
+    inputContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: 'gray',
+        borderWidth: 1,
+        borderRadius: 10,
+        marginTop: 5,
+    },
+    input: {
+        flex: 1,
+        height: 48,
+        padding: 10,
+    },
+    loginButton: {
+        backgroundColor: '#363062',
+        borderRadius: 10,
+        paddingVertical: 15,
+        alignItems: 'center',
+        marginTop: 20,
+    },
+    loginButtonText: {
+        color: '#fff',
+        fontSize: 18,
+        fontWeight: 'bold',
+    },})
